87d8a58c70e70e24dc182fdc516ac0c18a90ffd9
vijay <vijay@saturnsystems.com.my>
2014-07-11 17:32:07 +0800

    code for plot1

--- plot1.R
+++ plot1.R
@@ -1,7 +1,16 @@
 
+## read in the data from the "household_power_consumption.txt" file
 power = read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+## subset out the rquired dates
 power2 = subset(power, Date == "1/2/2007" | Date == "2/2/2007")
+
+## convert Global_active_power to numeric
 power2$Global_active_power = as.numeric(as.character(power2$Global_active_power))
+
+## create the histogram
 hist (power2$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+
+## save the plot
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
